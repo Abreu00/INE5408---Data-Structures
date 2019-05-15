db3724b53d927a2f435a38f6e355ec25cec42927
Fabio de Abreu <fabiowabreu@gmail.com>
2019-05-15 20:23:01 -0300

    Created functions to head image data from xml datasets on read_image.hpp file

---
+++ project_1/reads_image.hpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <fstream>
+#include "Image.h"
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+//! creates_image.hpp has the necessary functions to read the image
+//! from the xml file
+
+string getContent(string line, string tagName) {
+    
+    //! Returns content of xml element
+    //! Element has to be on line given otherwise it returns an empty string
+
+    string tag = '<' + tagName + '>';
+    
+    std::size_t start = line.find(tag);
+    std::size_t finish = line.find(tag.insert(1, "/"));
+    
+    if (start == string::npos || finish == string::npos) {
+        return "";
+    }
+    start += tag.length() - 1; //Position where tag data starts
+    finish -= 1; // Position where tag data finishes
+    return line.substr(start, finish - start + 1);
+}
+
+int** getImageBin(ifstream &file, string &line, int width, int height) {
+    if ( line !=  "<data>") {
+        throw invalid_argument("Line should be <data> in order to file read the matrix");
+    }
+
+    int** data = new int*[height];
+    char c = 'c';
+    for (int i = 0; i < height; ++i) {
+        data[i] = new int[width];
+        for (int j = 0; j < width; ++j) {
+            file >> c;
+            data[i][j] = c - '0';
+        }
+    }
+    return data;
+} 
+
+void dealocMatrix(int**& matrix, int lines, int cols) {
+    for (int i = 0; i < lines; ++i) {
+        delete [] matrix[i];
+    }
+    delete [] matrix;
+}
