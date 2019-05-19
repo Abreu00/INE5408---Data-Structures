c4f1faebae41aa7c67e26b25d5782e50f5dd4da1
Fabio de Abreu <fabiowabreu@gmail.com>
2019-05-18 22:30:08 -0300

    Separated methods definitions from declarations in order to improve readability

--- binary_search_tree/binary_tree.hpp
+++ binary_search_tree/binary_tree.hpp
@@ -5,174 +5,51 @@
 
 namespace structures {
 
-//! Classe Arvore binaria
+//! Classe Arvode binaria
 template<typename T>
 class BinaryTree {
  public:
-    ~BinaryTree() {
-        delete root;
-        size_ = 0u;
-    }
+    ~BinaryTree();
 
     //! Insere na arvore
-    void insert(const T& data) {
-        if (empty()) {
-            root = new Node(data);
-        } else {
-            root->insert(data);
-        }
-        size_++;
-    }
+    void insert(const T& data);
 
     //! Remove da arvore
-    void remove(const T& data) {
-        if (!empty()) {
-            if (root->remove(data)) --size_;
-        }
-    }
+    void remove(const T& data);
 
     //! Contém
-    bool contains(const T& data) const {
-        if (empty()) {
-            return false;
-        }
-        return root->contains(data);
-    }
+    bool contains(const T& data) const;
 
     //! Verifica se a arvore está vazia
-    bool empty() const {
-        return size_ == 0;
-    }
+    bool empty() const;
 
     //! Tamanho da arvore
-    std::size_t size() const {
-        return size_;
-    }
+    std::size_t size() const;
 
     //! pre-order
-    ArrayList<T> pre_order() const {
-        structures::ArrayList<T> v{};
-        if (!empty()) {
-            root->pre_order(v);
-        }
-        return v;
-    }
+    ArrayList<T> pre_order() const;
 
     //! em-ordem
-    ArrayList<T> in_order() const {
-        structures::ArrayList<T> v{};
-        if (!empty()) {
-            root->in_order(v);
-        }
-        return v;
-    }
+    ArrayList<T> in_order() const;
 
     //! pos-ordem
-    ArrayList<T> post_order() const {
-        structures::ArrayList<T> v{};
-        if (!empty()) {
-            root->post_order(v);
-        }
-        return v;
-    }
+    ArrayList<T> post_order() const;
 
  private:
     struct Node {
-        explicit Node(const T& value) {
-            data = value;
-        }
+        explicit Node(const T& value);
 
-        void insert(const T& value) {
-            if (value <= data) {
-                if (left == nullptr) {
-                    left = new Node(value);
-                } else {
-                    left->insert(value);
-                }
-            } else {
-                if (right == nullptr) {
-                    right = new Node(value);
-                } else {
-                    right->insert(value);
-                }
-            }
-        }
-        bool remove(const T& value) {
-            if (value == data) {
-                if ((left != nullptr) && (right != nullptr)) {
-                    Node *right_min = right;
-                    while (right_min->left) {
-                        right_min = right_min->left;
-                    }
-                    data = right_min->data;
-                    delete right_min;
-                    return true;
-                } else {
-                    if (right != nullptr) {
-                        data = right->data;
-                        return right->remove(data);
-                    } else if (left != nullptr) {
-                        data = left->data;
-                        return left->remove(data);
-                    } else {  // Leaf Node
-                            delete this;
-                            return true;
-                    }
-                }
-            } else {
-                if (right != nullptr && data < value) {
-                    return right->remove(value);
-                } else if (left != nullptr && data > value) {
-                    return left->remove(value);
-                }
-            }
-            return false;
-        }
+        void insert(const T& value);
 
-        bool contains(const T& value) const {
-            if (value == data) {
-                return true;
-            } else if (value <= data) {
-                if (left != nullptr) {
-                    return left->contains(value);
-                }
-            } else {
-                if (right != nullptr) {
-                    return right->contains(value);
-                }
-            }
-            return false;  // right or left is nullptr
-        }
+        bool remove(const T& value);
 
-        void pre_order(ArrayList<T>& v) const {
-            v.push_back(data);
-            if (left != nullptr) {
-                left->pre_order(v);
-            }
-            if (right != nullptr) {
-                right->pre_order(v);
-            }
-        }
+        bool contains(const T& value) const;
 
-        void in_order(ArrayList<T>& v) const {
-            if (left != nullptr) {
-                left->in_order(v);
-            }
-            v.push_back(data);
-            if (right != nullptr) {
-                right->in_order(v);
-            }
-        }
+        void pre_order(ArrayList<T>& v) const;
 
-        void post_order(ArrayList<T>& v) const {
-            if (left != nullptr) {
-                left->post_order(v);
-            }
-            if (right != nullptr) {
-                right->post_order(v);
-            }
-            v.push_back(data);
-        }
+        void in_order(ArrayList<T>& v) const;
+
+        void post_order(ArrayList<T>& v) const;
 
         T data;
         Node* left{nullptr};
@@ -183,5 +60,180 @@ class BinaryTree {
     std::size_t size_{0u};
 };
 }  // namespace structures
+
+template<typename T>
+structures::BinaryTree<T>::~BinaryTree() {
+    delete root;
+    size_ = 0u;
+}
+
+template<typename T>
+void structures::BinaryTree<T>::insert(const T& data) {
+    if (empty()) {
+        root = new Node(data);
+    } else {
+        root->insert(data);
+    }
+    size_++;
+}
+
+template<typename T>
+void structures::BinaryTree<T>::remove(const T& data) {
+    if (!empty()) {
+        if (root->remove(data)) --size_;
+    }
+}
+
+template<typename T>
+bool structures::BinaryTree<T>::contains(const T& data) const {
+    if (empty()) {
+        return false;
+    }
+    return root->contains(data);
+}
+
+template<typename T>
+structures::ArrayList<T> structures::BinaryTree<T>::in_order() const {
+    structures::ArrayList<T> v{};
+    if (!empty()) {
+        root->in_order(v);
+    }
+    return v;
+}
+
+template<typename T>
+structures::ArrayList<T> structures::BinaryTree<T>::pre_order() const {
+    structures::ArrayList<T> v{};
+    if (!empty()) {
+        root->pre_order(v);
+    }
+    return v;
+}
+
+template<typename T>
+structures::ArrayList<T> structures::BinaryTree<T>::post_order() const {
+    structures::ArrayList<T> v{};
+    if (!empty()) {
+        root->post_order(v);
+    }
+    return v;
+}
+
+template<typename T>
+std::size_t structures::BinaryTree<T>::size() const {
+    return size_;
+}
+
+template<typename T>
+bool structures::BinaryTree<T>::empty() const {
+    return size_ == 0;
+}
+
+/* ======== Node methods ======== */
+
+template<typename T>
+structures::BinaryTree<T>::Node::Node(const T& value) {
+    data = value;
+}
+
+template<typename T>
+bool structures::BinaryTree<T>::Node::contains(const T& value) const {
+    if (value == data) {
+        return true;
+    } else if (value <= data) {
+        if (left != nullptr) {
+            return left->contains(value);
+        }
+    } else {
+        if (right != nullptr) {
+            return right->contains(value);
+        }
+    }
+    return false;  // right or left is nullptr
+}
+
+template<typename T>
+void structures::BinaryTree<T>::Node::insert(const T& value) {
+    if (value <= data) {
+        if (left == nullptr) {
+            left = new Node(value);
+        } else {
+            left->insert(value);
+        }
+    } else {
+        if (right == nullptr) {
+            right = new Node(value);
+        } else {
+            right->insert(value);
+        }
+    }
+}
+
+
+template<typename T>
+bool structures::BinaryTree<T>::Node::remove(const T& value) {
+    if (value == data) {
+        if ((left != nullptr) && (right != nullptr)) {
+            Node *right_min = right;
+            while (right_min->left) {
+                right_min = right_min->left;
+            }
+            data = right_min->data;
+            delete right_min;
+            return true;
+        } else {
+            if (right != nullptr) {
+                data = right->data;
+                return right->remove(data);
+            } else if (left != nullptr) {
+                data = left->data;
+                return left->remove(data);
+            } else {  // Leaf Node
+                    delete this;
+                    return true;
+            }
+        }
+    } else {
+        if (right != nullptr && data < value) {
+            return right->remove(value);
+        } else if (left != nullptr && data > value) {
+            return left->remove(value);
+        }
+    }
+    return false;
+}
+
+template<typename T>
+void structures::BinaryTree<T>::Node::pre_order(structures::ArrayList<T>& v) const {
+    v.push_back(data);
+    if (left != nullptr) {
+        left->pre_order(v);
+    }
+    if (right != nullptr) {
+        right->pre_order(v);
+    }
+}
+
+template<typename T>
+void structures::BinaryTree<T>::Node::in_order(structures::ArrayList<T>& v) const {
+    if (left != nullptr) {
+        left->in_order(v);
+    }
+    v.push_back(data);
+    if (right != nullptr) {
+        right->in_order(v);
+    }
+}
+
+template<typename T>
+void structures::BinaryTree<T>::Node::post_order(structures::ArrayList<T>& v) const {
+    if (left != nullptr) {
+        left->post_order(v);
+    }
+    if (right != nullptr) {
+        right->post_order(v);
+    }
+    v.push_back(data);
+}
 
 #endif
